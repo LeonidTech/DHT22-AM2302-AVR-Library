79261702f29566234eb830689a95fe94d283a110
LeonidTech <Ender366@gmail.com>
2017-01-26 01:49:51 -0500

    Add files via upload
    Initial upload... polling of single port pin. 4 defines in header need to be set. Some people have reported timings out of spec for their devices, if issues persist move device close to MCU with pull-up resistor and adjust timings in Cpp file if necessary. Short description of how to use the library is in the header file.

---
+++ DHT22_AM2302.cpp
@@ -0,0 +1,123 @@
+/*
+DHT22 & AM2302 Library
+ Author : Leonid (DLM)
+*/
+
+#ifndef F_CPU
+#define F_CPU 8000000UL
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+#include "DHT22_AM2302.h"
+
+//Default Constructor
+DHT22::DHT22()
+{
+	temperature = humidity = 0;
+}
+
+// get data from sensor
+int8_t DHT22::readData()
+{
+	uint8_t bits[5];
+	uint8_t i,j = 0;
+
+	memset(bits, 0, sizeof(bits));
+
+	//reset port
+	DHT_DDR |= (1<<DHT_INPUTPIN); //output
+	DHT_PORT |= (1<<DHT_INPUTPIN); //high
+	_delay_ms(100);
+
+	//send request
+	DHT_PORT &= ~(1<<DHT_INPUTPIN); //low
+	_delay_us(500);
+	DHT_PORT |= (1<<DHT_INPUTPIN); //high
+	DHT_DDR &= ~(1<<DHT_INPUTPIN); //input
+	_delay_us(40);
+
+	//check start condition 1
+	if((DHT_PIN & (1<<DHT_INPUTPIN))) {
+		return -1;
+	}
+	_delay_us(80);
+	
+	//check start condition 2
+	if(!(DHT_PIN & (1<<DHT_INPUTPIN))) {
+		return -1;
+	}
+	_delay_us(80);
+
+	//read the data
+	uint16_t timeoutcounter = 0;
+	for (j=0; j<5; j++) { //read 5 byte
+		uint8_t result=0;
+		for(i=0; i<8; i++) {//read every bit
+			timeoutcounter = 0;
+			while(!(DHT_PIN & (1<<DHT_INPUTPIN))) { //wait for an high input (non blocking)
+				timeoutcounter++;
+				if(timeoutcounter > DHT_TIMEOUT) {
+					return -1; //timeout
+				}
+			}
+			_delay_us(30);
+			if(DHT_PIN & (1<<DHT_INPUTPIN)) //if input is high after 30 us, get result
+			result |= (1<<(7-i));
+			timeoutcounter = 0;
+			while(DHT_PIN & (1<<DHT_INPUTPIN)) { //wait until input get low (non blocking)
+				timeoutcounter++;
+				if(timeoutcounter > DHT_TIMEOUT) {
+					return -1; //timeout
+				}
+			}
+		}
+		bits[j] = result;
+	}
+
+	//reset port
+	DHT_DDR |= (1<<DHT_INPUTPIN); //output
+	DHT_PORT |= (1<<DHT_INPUTPIN); //low
+	_delay_ms(100);
+
+	//check checksum
+	if ((uint8_t)(bits[0] + bits[1] + bits[2] + bits[3]) == bits[4])
+	{
+		//return temperature and humidity
+		uint16_t rawhumidity = bits[0]<<8 | bits[1];
+		uint16_t rawtemperature = bits[2]<<8 | bits[3];
+		if(rawtemperature & 0x8000)
+		{
+			temperature = (float)((rawtemperature & 0x7FFF) / 10.0) * -1.0;
+		}
+		else
+		{
+			temperature = (float)(rawtemperature)/10.0;
+		}
+		humidity = (float)(rawhumidity)/10.0;
+		return 0;
+	}
+
+	return -1;
+}
+
+//get temperature in Celsius
+float DHT22::gettemperatureC()
+{
+	return temperature;
+}
+
+//get Humidity
+float DHT22::gethumidity()
+{
+	return humidity;
+}
+
+//get temperature in Fahrenheit 
+float DHT22::gettemperatureF()
+{
+	return (temperature * 1.8) + 32; //Return temp in F
+}
